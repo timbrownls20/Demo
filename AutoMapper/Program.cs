6d825d534e722e8a0eb1af09f231f0debfc04965
timbrownls20 <timbrownls26@hotmail.com>
2016-09-19 19:28:33 +0100

    refactor

--- AutoMapper/Program.cs
+++ AutoMapper/Program.cs
@@ -13,11 +13,7 @@ namespace Automapper
     {
         static void Main(string[] args)
         {
-            // Initialize database
-            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
-            Database.SetInitializer(new DemoInitialiser());
-            DemoContext context = new DemoContext();
-            context.Database.Initialize(true);
+            InitDatabase();
 
             var keyIn = ShowMenu();
 
@@ -26,90 +22,112 @@ namespace Automapper
                 //.. set up mappings
                 if (keyIn.Key == ConsoleKey.NumPad1 || keyIn.Key == ConsoleKey.D1)
                 {
-            
-                    var customerRepository = new CustomerRepository();
-                    var customer = customerRepository.FindById(1);
-
-                    Mapper.Initialize(cfg =>
-                    {
-                        cfg.CreateMap<Customer, CustomerDto>();
-
-                        cfg.CreateMap<Address, AddressDto>()
-                            .ForMember(d => d.Residential, o => o.Ignore());
-                    });
-
-                    //..validate mappings
-                    Mapper.AssertConfigurationIsValid();
-
-                    var customerDto = Mapper.Map<CustomerDto>(customer);
-                    Console.WriteLine(customerDto.ToString());
+                    MapSingleObjectNoParameters();
                 }
                 else if (keyIn.Key == ConsoleKey.NumPad2 || keyIn.Key == ConsoleKey.D2)
                 {
-                    var customerRepository = new CustomerRepository();
-                    var customer = customerRepository.FindById(1);
-
-                    Mapper.Initialize(cfg =>
-                    {
-                        cfg.CreateMap<Customer, CustomerDto>();
-                        
-                        cfg.CreateMap<Address, AddressDto>()
-                            .ForMember(d => d.Residential,
-                                o =>
-                                    o.ResolveUsing(
-                                        (src, dest, destMember, resContext) =>
-                                            dest.Residential =
-                                                (CustomerType) resContext.Items["CustomerType"] != CustomerType.Business))
-                            ;
-                    });
-
-                    //..validate mappings
-                    Mapper.AssertConfigurationIsValid();
-
-                    var customerDto = Mapper.Map<CustomerDto>(customer,
-                        opts => opts.Items["CustomerType"] = customer.CustomerType);
-                    Console.WriteLine(customerDto.ToString());
+                    MapSingleObjectWithParameters();
                 }
                 else if (keyIn.Key == ConsoleKey.NumPad3 || keyIn.Key == ConsoleKey.D3)
                 {
-                    var customerRepository = new CustomerRepository();
-                    var customers = customerRepository.ListAll();
+                    MapListOfObjects();
 
-                    Mapper.Initialize(cfg =>
-                    {
-                        cfg.CreateMap<Customer, CustomerDto>()
-                        .BeforeMap((customer, customerDto, resContext) =>
-                        {
-                            resContext.Items["CustomerType"] = customer.CustomerType;
-                        });
-
-                        cfg.CreateMap<Address, AddressDto>()
-                            .ForMember(d => d.Residential,
-                                o =>
-                                    o.ResolveUsing(
-                                        (src, dest, destMember, resContext) =>
-                                            dest.Residential =
-                                                (CustomerType) resContext.Items["CustomerType"] != CustomerType.Business))
-                            ;
-                    });
-
-                    //..validate mappings
-                    Mapper.AssertConfigurationIsValid();
-
-                    var customerDtos = Mapper.Map<List<CustomerDto>>(customers);
-                    foreach (var customerDto in customerDtos)
-                    {
-                        Console.WriteLine(customerDto.ToString()); 
-                        Console.WriteLine();   
-                    }
-                    
                 }
 
                 keyIn = ShowMenu();
             }
 
-            //.. pause at end
-            //Console.ReadKey(true);
+
+        }
+
+        private static void InitDatabase()
+        {
+            // Initialize database
+            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
+            Database.SetInitializer(new DemoInitialiser());
+            DemoContext context = new DemoContext();
+            context.Database.Initialize(true);
+        }
+
+        private static void MapListOfObjects()
+        {
+            var customerRepository = new CustomerRepository();
+            var customers = customerRepository.ListAll();
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Customer, CustomerDto>()
+                .BeforeMap((customer, customerDto, resContext) =>
+                {
+                    resContext.Items["CustomerType"] = customer.CustomerType;
+                });
+
+                cfg.CreateMap<Address, AddressDto>()
+                    .ForMember(d => d.Residential,
+                        o =>
+                            o.ResolveUsing(
+                                (src, dest, destMember, resContext) =>
+                                    dest.Residential =
+                                        (CustomerType)resContext.Items["CustomerType"] != CustomerType.Business))
+                    ;
+            });
+
+            //..validate mappings
+            Mapper.AssertConfigurationIsValid();
+
+            var customerDtos = Mapper.Map<List<CustomerDto>>(customers);
+            foreach (var customerDto in customerDtos)
+            {
+                Console.WriteLine(customerDto.ToString());
+                Console.WriteLine();
+            }
+        }
+
+        private static void MapSingleObjectWithParameters()
+        {
+            var customerRepository = new CustomerRepository();
+            var customer = customerRepository.FindById(1);
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Customer, CustomerDto>();
+
+                cfg.CreateMap<Address, AddressDto>()
+                    .ForMember(d => d.Residential,
+                        o =>
+                            o.ResolveUsing(
+                                (src, dest, destMember, resContext) =>
+                                    dest.Residential =
+                                        (CustomerType)resContext.Items["CustomerType"] != CustomerType.Business))
+                    ;
+            });
+
+            //..validate mappings
+            Mapper.AssertConfigurationIsValid();
+
+            var customerDto = Mapper.Map<CustomerDto>(customer,
+                opts => opts.Items["CustomerType"] = customer.CustomerType);
+            Console.WriteLine(customerDto.ToString());
+        }
+
+        private static void MapSingleObjectNoParameters()
+        {
+            var customerRepository = new CustomerRepository();
+            var customer = customerRepository.FindById(1);
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Customer, CustomerDto>();
+
+                cfg.CreateMap<Address, AddressDto>()
+                    .ForMember(d => d.Residential, o => o.Ignore());
+            });
+
+            //..validate mappings
+            Mapper.AssertConfigurationIsValid();
+
+            var customerDto = Mapper.Map<CustomerDto>(customer);
+            Console.WriteLine(customerDto.ToString());
         }
 
         private static ConsoleKeyInfo ShowMenu()
