add2e8541574b691653a882a836f963b61650b9b
timbrownls20 <timbrownls26@hotmail.com>
2017-03-25 10:36:56 +0000

    xamarin hello world

---
+++ Xamarin/Demo1/Demo1/SquareRootPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo1
+{
+    public partial class SquareRootPage : ContentPage
+    {
+        public SquareRootPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnCalculate(object sender, EventArgs e)
+        {
+            //this.lblOutput.Text = "Button clicked";
+            int numberIn;
+
+            if (Int32.TryParse(this.txtNumber.Text, out numberIn))
+            {
+
+            }
+        }
+    }
+}
