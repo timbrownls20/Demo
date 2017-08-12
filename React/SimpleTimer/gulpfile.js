b274f175779ee92de9271727f8e455126dc0e851
timbrownls20 <timbrownls26@hotmail.com>
2017-08-12 22:32:06 +1000

    React demo first pass

---
+++ React/SimpleTimer/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+// gulp.task('default', function(){
+//     console.log('Gulp: Hello world');
+//     }
+// );
+
+gulp.task('default', function(){
+        
+        return browserify('./source/app.js')
+                .transform(babelify)
+                .bundle()
+                .pipe(source('simpletimer.js'))
+                .pipe(gulp.dest('./build/'));
+    }
+);
---
+++ React/SimpleTimer/source/app.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var reactElement = <h1 className="header">
+                     This is a JSX element
+                   </h1>;
+
+
+
+ReactDOM.render(reactElement, document.getElementById('react-application'));
