3e3e19b7ec31cafa4aa6bdaf79f32a890ebb442d
timbrownls20 <timbrownls26@hotmail.com>
2017-08-29 16:56:41 +1000

    Passing data to parent component on click

--- React/TodoList/source/app.js
+++ React/TodoList/source/app.js
@@ -1,10 +1,23 @@
+var util = require('util')
 var React = require('react');
 var ReactDOM = require('react-dom');
 var TodoList = require('./components/todolist.react.js');
 var FullRow = require('./components/fullrow.react.js');
 var Label = require('./components/label.react.js');
+// var $ = require("jquery")(window);
+
 
 class Application extends React.Component{
+
+
+  addTask(input){
+
+    //console.log("[Application] addTask e:" + util.inspect(this.refs));
+    console.log("[Application] addTask debug:" + input);
+    
+
+
+  }
 
 
   //.. life cycle events
@@ -15,6 +28,8 @@ class Application extends React.Component{
     {
        todoitems :{}
     };
+
+    this.addTask = this.addTask.bind(this);
   }
 
   componentWillMount(){
@@ -41,6 +56,7 @@ class Application extends React.Component{
                   <h1>{this.props.label}</h1>
               </FullRow>
               <TodoList todoitems={this.state.todoitems} />
+              <TodoInsert addTask={this.addTask}/>
               <Label label={JSON.stringify(this.state)} visible="false"/>
             </div>;
            
@@ -49,6 +65,47 @@ class Application extends React.Component{
 };
 
 
+class TodoInsert extends React.Component{
 
+
+  //.. life cycle events
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  //handleClick = () => {
+  handleClick(){
+
+    //console.log(util.inspect(this.textInput.value));
+
+    this.props.addTask(this.textInput.value);
+  }
+
+
+   render(){
+
+    console.log('[TodoInsert] render');
+   
+    return  <fieldset className="form-group">
+              <div className="row">
+                <div className="col-lg-12">
+                    <small htmlFor="addItem" className="col-form-label text-muted">Add Task</small>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-lg-10">
+                  <input ref={(input) => { this.textInput = input; console.log('inserting ref')}} className="form-control" type="text" defaultValue="New Task" id="newTask"  />
+                </div>
+                <div className="col-lg-2">
+                  <button onClick={this.handleClick} className="btn btn-primary">Add Task</button>
+                </div>
+              </div>
+            </fieldset>;
+           
+  } 
+
+}
 
 ReactDOM.render(<Application label="TODO"/>, document.getElementById('react-application'));
