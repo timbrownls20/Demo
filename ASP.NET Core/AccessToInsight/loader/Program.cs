8fb9611c85f0dbed72344f99e4825598d8d168cf
timbrownls20 <timbrownls26@hotmail.com>
2017-10-01 16:45:47 +1000

    dot net console app with mongo db integrated

---
+++ ASP.NET Core/AccessToInsight/loader/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace loader
+{
+    class Program
+    {
+        public const string MONGODB_CONNECTION = "mongodb://localhost:27017";
+
+        public static void Main(string[] args)
+        {
+            ListDatabases();
+        }
+
+        public static async void ListDatabases()
+        {
+            var client = new MongoClient(MONGODB_CONNECTION);
+            using (var cursor = await client.ListDatabasesAsync())
+            {
+                await cursor.ForEachAsync(d => Console.WriteLine(d.ToString()));
+            }
+        }
+    }
+}
