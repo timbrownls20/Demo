4d028266d52ccbca9c8b1bf027def5c4db4a6def
timbrownls20 <timbrownls26@hotmail.com>
2017-10-02 10:44:01 +1000

    source files for import

--- ASP.NET Core/AccessToInsight/loader/Program.cs
+++ ASP.NET Core/AccessToInsight/loader/Program.cs
@@ -9,12 +9,16 @@ namespace loader
 
         public static void Main(string[] args)
         {
+            Console.WriteLine("Loader on");
             ListDatabases();
         }
 
         public static async void ListDatabases()
         {
             var client = new MongoClient(MONGODB_CONNECTION);
+            client.DropDatabase("BookShelf");
+            //client.GetDatabase()
+
             using (var cursor = await client.ListDatabasesAsync())
             {
                 await cursor.ForEachAsync(d => Console.WriteLine(d.ToString()));
