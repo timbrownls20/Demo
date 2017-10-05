6c34d5abccaee2c9b2daea8a891dcc8c64ff0161
timbrownls20 <timbrownls26@hotmail.com>
2017-10-05 17:10:01 +1000

    add all variants to import demo

--- React/CommonJSDemo/client/components/Page1.jsx
+++ React/CommonJSDemo/client/components/Page1.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import {Info, Warning, Danger} from './Labels.jsx'
+
+//.. import variant 1
+var Info = require('./Labels.jsx').Info;
+var Warning = require('./Labels.jsx').Warning;
+var Danger = require('./Labels.jsx').Danger;
+
+
+//.. import variant 2
+//var {Info, Warning, Danger} = require('./Labels.jsx')
+
+//..import variant 3
+//import {Info, Warning, Danger} from './Labels.jsx'
 
 
 export default class Page1 extends React.Component {
