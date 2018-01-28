e02131709204e13c243d429d08b3bb0604a92ee4
timbrownls20 <timbrownls26@hotmail.com>
2018-01-28 17:25:36 +1000

    diagonal checking

--- Angular/noughts-and-crosses/src/app/services/game.service.ts
+++ Angular/noughts-and-crosses/src/app/services/game.service.ts
@@ -24,63 +24,54 @@ export class GameService {
 
   updateGameResult(){
 
-    var gridDimension: number[] = [1,2,3]
+    const gridDimension: number[] = [1,2,3]
 
-    debugger;
-
-    //let app = this;
-
+    let resultSetDiagonal1: Cell[] = new Array<Cell>();
+    let resultSetDiagonal2: Cell[] = new Array<Cell>();
+    
     for(let i of gridDimension){
 
-      let resultSetRow: Cell[] = new Array<Cell>();
-      let resultSetColumn: Cell[] = new Array<Cell>();
-      
-
-      for(let j of gridDimension){
+        let resultSetRow: Cell[] = new Array<Cell>();
+        let resultSetColumn: Cell[] = new Array<Cell>();
         
-        let rowCell:Cell = this.model.state[new Cell(i, j).index];
-        if(rowCell !== undefined)
-          resultSetRow.push(rowCell);
+        let diagonal1Cell:Cell = this.model.state[new Cell(i, i).index];
+        if(diagonal1Cell !== undefined)
+          resultSetDiagonal1.push(diagonal1Cell);
 
-        let columnCell:Cell = this.model.state[new Cell(j, i).index];
-        if(columnCell !== undefined)
-          resultSetColumn.push(columnCell);
+        let diagonal2Cell:Cell = this.model.state[new Cell(i, 4-i).index];
+          if(diagonal2Cell !== undefined)
+            resultSetDiagonal2.push(diagonal2Cell);
+
+        for(let j of gridDimension){
+          
+          let rowCell:Cell = this.model.state[new Cell(i, j).index];
+          if(rowCell !== undefined)
+            resultSetRow.push(rowCell);
+
+          let columnCell:Cell = this.model.state[new Cell(j, i).index];
+          if(columnCell !== undefined)
+            resultSetColumn.push(columnCell);
+        }
+
+        this.checkResultSet(resultSetRow);
+        this.checkResultSet(resultSetColumn);
+
       }
 
-      this.checkResultSet(resultSetRow);
-      this.checkResultSet(resultSetColumn);
-
-      // if(resultSet.length === 3){
-        
-      // if(resultSet[0].state === resultSet[1].state && resultSet[1].state === resultSet[2].state){
-
-      //     if(resultSet[0].state === CellState.Cross)
-      //       this.model.result = GameResult.CrossesWin;
-      //     else
-      //       this.model.result = GameResult.NoughtsWin;
-      //   }
-
-      // }
-
-     
-    }
-
+      this.checkResultSet(resultSetDiagonal1);
+      this.checkResultSet(resultSetDiagonal2);
   }
 
   private checkResultSet(resultSet: Cell[]): void{
 
-    //let result: GameResult = GameResult.InProgress;
-
-    if(resultSet.length === 3){
+    if(resultSet.length !== 3) return;
       
-      if(resultSet[0].state === resultSet[1].state && resultSet[1].state === resultSet[2].state){
+    if(resultSet[0].state === resultSet[1].state && resultSet[1].state === resultSet[2].state){
 
-          if(resultSet[0].state === CellState.Cross)
-            this.model.result = GameResult.CrossesWin;
-          else
-            this.model.result = GameResult.NoughtsWin;
-        }
-
+        if(resultSet[0].state === CellState.Cross)
+          this.model.result = GameResult.CrossesWin;
+        else
+          this.model.result = GameResult.NoughtsWin;
       }
 
   }
