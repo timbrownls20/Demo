dfb1721866e4310737aa1431c3530fc21dfe99f8
timbrownls20 <timbrownls26@hotmail.com>
2021-07-30 16:56:07 +1000

    workout planner base build

---
+++ React/workout-planner/pages/_app.js
@@ -0,0 +1,9 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
