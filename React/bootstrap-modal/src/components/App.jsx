b775dba4a33be86dfdf944b31ab7b41f2ed47601
timbrownls20 <timbrownls26@hotmail.com>
2021-08-08 12:30:28 +1000

    modal refactor and add to UI to show transparency

--- React/bootstrap-modal/src/components/App.jsx
+++ React/bootstrap-modal/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
 import ModalLauncher from "./ModalLauncher";
+import '../css/modal.css';
 
 function App() {
   const [show, setShow] = useState(false);
@@ -16,7 +17,15 @@ function App() {
   return (
     <div>
       <div className="container-fluid">
-        <ModalLauncher showModal={showModal} />
+      <div className="d-flex justify-content-center align-content-center m-5">
+            <div className="p-5 demo-text">
+            Call me Ishmael. Some years ago - never mind how long precisely - having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.
+            </div>
+            <ModalLauncher showModal={showModal} />
+            <div className="p-5 demo-text">
+            Marley was dead, to begin with. There is no doubt whatever about that. The register of his burial was signed by the clergyman, the clerk, the undertaker, and the chief mourner. 
+            </div>
+        </div>
         <Modal show={show} hideModal={hideModal}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </Modal>
--- React/bootstrap-modal/src/components/ModalLauncher.jsx
+++ React/bootstrap-modal/src/components/ModalLauncher.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 
 // eslint-disable-next-line react/prop-types
-const ModalLauncher = ({showModal}) => {
-    return (
-        <div className="d-flex justify-content-center align-content-center m-5">
-            <div className="card text-center p-5 modal-launch">
-                <div className="card-body">
-                    <div className="card-title">
-                        <h5>Bootstrap Modal</h5>
-                    </div>
-                    <div className="card-text text-left m-3">
-                        React bootstrap modal demonstration with no JQuery or react
-                        bootstrap components
-                    </div>
-                    <button
-                        type="button"
-                        className="btn btn-outline-primary mt-3"
-                        onClick={showModal}
-                    >
-                        Open Modal
-                    </button>
-                </div>
-            </div>
+const ModalLauncher = ({ showModal }) => {
+  return (
+    <div className="card text-center p-5 modal-launch">
+      <div className="card-body">
+        <div className="card-title">
+          <h5>Bootstrap Modal</h5>
         </div>
-    );
-}
+        <div className="card-text text-left m-3">
+          React bootstrap modal demonstration with no JQuery or react bootstrap
+          components
+        </div>
+        <button
+          type="button"
+          className="btn btn-outline-primary mt-3"
+          onClick={showModal}
+        >
+          Open Modal
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default ModalLauncher;
