8831a62aa305ab4d939132033d30e322f23eab5b
timbrownls20 <timbrownls26@hotmail.com>
2022-01-25 16:52:29 +1000

    simple timer initial commit

--- ReactNative/SimpleTimer/App.tsx
+++ ReactNative/SimpleTimer/App.tsx
@@ -75,7 +75,7 @@ const App = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits. TB DEBUG 1
+            screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
