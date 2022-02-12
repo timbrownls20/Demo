6bb3eba9d04842928aec173cbf79b85ec3e20c0e
timbrownls20 <timbrownls26@hotmail.com>
2022-02-12 16:06:18 +1000

    correct add

--- Node/npm-link/master-project/index.js
+++ Node/npm-link/master-project/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const News = require("news");
+const Weather = require("weather");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   const news = News();
+  const weather = Weather();
 
   res.send(`
   Brisbane
-  News: ${news.Today()}`);
+  News: ${news.Today()}
+  Weather: ${weather.Today()}
+  `);
 });
 
 app.listen(port, () => {
---
+++ Node/npm-link/weather/index.js
@@ -0,0 +1,12 @@
+const Weather = () => {
+  const data = ["Hot", "Hot and sunny", "Very hot", "Hot hot hot", "Sunny", "Hot and rainy"];
+
+  const Today = () => {
+    const index = Math.floor(Math.random() * data.length);
+    return data[index];
+  };
+
+  return { Today };
+};
+
+module.exports = Weather;
