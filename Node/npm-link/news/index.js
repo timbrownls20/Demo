9ff2e5f81bd379b3b688c1dda7700898e0145a6d
timbrownls20 <timbrownls26@hotmail.com>
2022-02-12 15:40:36 +1000

    multiple news messages

--- Node/npm-link/news/index.js
+++ Node/npm-link/news/index.js
@@ -1,9 +1,12 @@
 const News = () => {
+  const data = ["All Good", "Awesome", "No Worries", "No Dramas"];
+
   const Today = () => {
-    return "All Good";
+    const index = Math.floor(Math.random() * data.length);
+    return data[index];
   };
 
   return { Today };
 };
 
-module.exports = News ;
+module.exports = News;
